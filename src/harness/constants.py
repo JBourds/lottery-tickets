cdb92057cddbb52671398b94eed364abf272555b
Jordan Bourdeau <jordan.bourdeau@uvm.edu>
2024-04-21 19:25:51 -0400

    Main training loop seems to be more or less working.

--- src/harness/constants.py
+++ src/harness/constants.py
@@ -36,10 +36,11 @@ class Constants:
     # Training Parameters
     MNIST_LOCATION: str = DATA_DIRECTORY + 'mnist/'
 
-    PATIENCE: int = 3
-    MINIMUM_DELTA: float = 0.01
+    PATIENCE: int = 10
+    MINIMUM_DELTA: float = 0.005
     LEARNING_RATE: float = 0.1
     OPTIMIZER = functools.partial(tf.keras.optimizers.legacy.Adam, LEARNING_RATE)
+    LOSS_FUNCTION: tf.keras.losses.Loss = functools.partial(tf.keras.losses.CategoricalCrossentropy)
 
     # Test Experiment Parameters
     TEST_NUM_MODELS: int = 2
@@ -49,4 +50,5 @@ class Constants:
     # Real Experiment Parameters
     NUM_MODELS: int = 100
     TRAINING_EPOCHS: int = 30
+    BATCH_SIZE: int = 128
     PRUNING_STEPS: int = 45
--- src/harness/training.py
+++ src/harness/training.py
@@ -71,8 +71,9 @@ def train_one_step(
     mask_model: tf.keras.Model, 
     inputs: tf.Tensor, 
     labels: tf.Tensor, 
-    loss_fn: tf.keras.losses.Loss, 
     optimizer: tf.keras.optimizers.Optimizer,
+    loss_fn: tf.keras.metrics.Metric = C.LOSS_FUNCTION(),
+    accuracy: tf.keras.metrics.Metric = tf.keras.metrics.CategoricalAccuracy(),
     ):
     """
     Tensorflow function to performa a single step of gradient descent.
@@ -82,12 +83,15 @@ def train_one_step(
                        the mask being applied to the model getting trained.
     :param inputs:     Batch inputs.
     :param labels:     Batch labels.
-    :param loss_fn:    Loss function being used.
-    :param optimizer:  Optimizer function being used,
+    :param optimizer:  Optimizer function being used. 
+    :param loss_fn:    Loss function being used. Defaults to value in `constants.py`.
+    :param accuracy:   Accuracy metric to be used. Defaults to value in `constants.py`.
+
+    :returns: Loss and accuracy from data passed into the model.
     """
     with tf.GradientTape() as tape:
-        y_pred: tf.Tensor = model(inputs)
-        loss: tf.keras.losses.Loss = loss_fn(labels, y_pred)
+        predictions: tf.Tensor = model(inputs)
+        loss: tf.keras.losses.Loss = loss_fn(labels, predictions)
         
     gradients: tf.Tensor = tape.gradient(loss, model.trainable_variables)
 
@@ -98,13 +102,15 @@ def train_one_step(
 
     optimizer.apply_gradients(zip(grad_mask_mul, model.trainable_variables))
 
+    return loss, accuracy(labels, predictions)
+
 @tf.function(experimental_relax_shapes=True)
 def test_step(
     model: tf.keras.Model, 
     inputs: tf.Tensor, 
     labels: tf.Tensor,
-    test_loss: tf.keras.metrics.Metric,
-    test_accuracy: tf.keras.metrics.Metric,
+    loss_fn: tf.keras.metrics.Metric = C.LOSS_FUNCTION(),
+    accuracy: tf.keras.metrics.Metric = tf.keras.metrics.CategoricalAccuracy(),
     ) -> tuple[float, float]:
     """
     Function to test model performance on testing dataset.
@@ -112,101 +118,127 @@ def test_step(
     :param model:      Keras model being tested.
     :param inputs:     Testing inputs.
     :param labels:     Testing labels.
-    :param loss_fn:    Loss function being used.
+    :param loss_fn:    Loss function being used. Defaults to value in `constants.py`.
+    :param accuracy:   Accuracy metric to be used. Defaults to value in `constants.py`.
+
+    :returns: Loss and accuracy from data passed into the model.
     """
     predictions: tf.Tensor = model(inputs)
-    loss: float = test_loss(labels, predictions)
-    accuracy: float = test_accuracy(labels, predictions)
-    return loss, accuracy
+    return loss_fn(labels, predictions), accuracy(labels, predictions)
 
 # Training function
 def training_loop(
         pruning_step: int,
-        model_stripped: tf.keras.Model, 
-        mask_model_stripped: tf.keras.Model,
+        model: tf.keras.Model, 
+        mask_model: tf.keras.Model,
         make_dataset: callable,
-        make_train_test_loss_accuracy: callable,
-        num_epochs: int, 
-        patience: int,
-        minimum_delta: float,
+        num_epochs: int = C.TRAINING_EPOCHS, 
+        batch_size: int = C.BATCH_SIZE,
+        patience: int = C.PATIENCE,
+        minimum_delta: float = C.MINIMUM_DELTA,
+        loss_fn: tf.keras.losses.Loss = C.LOSS_FUNCTION(),
         optimizer: tf.keras.optimizers.Optimizer = C.OPTIMIZER(),
-    ) -> tf.keras.Model:
+        allow_early_stopping: bool = True,
+    ) -> tuple[tf.keras.Model, TrainingRound]:
     """
     Main training loop for the model.
+
+    :param pruning_step:  Integer for the # pruning step the model is on. Used for saving model weights.
+    :param model:         Keras model with weights being trained for performance.  
+    :param mask_model:    Keras model whose weights signify the masks to use on gradient updates. 
+    :param make_dataset:  Function which produces the inputs/labels.
+    :param num_epochs:    Integer value for the number of epochs to run. Has a default value in `constants.py`.
+    :param batch_size:    Size of batches to train on. Has a default value in `constants.py`.
+    :param patience:      Number of rounds before a model is considered to no longer be improving for early stopping. 
+                          Has a default value in `constants.py`.
+    :param minimum_delta: Minimum accuracy improvement to be considered an improvement.
+                          Has a default value in `constants.py`.
+    :param loss_fn:       Loss function being used. Has a default value in `constants.py`.
+    :param optimizer:     Optimizer function being used to update model weights. Has a default value in `constants.py`.
+
+    :returns: Model with updated weights as well as the training round data.
     """
     # Number of epochs without improvement
     local_patience: int = 0
     best_test_loss: float = float('inf')
 
-    # Create metrics
-    train_loss, train_accuracy, test_loss, test_accuracy = get_train_test_loss_accuracy()
-
     # Extract input and target
-    X_train, Y_train, X_test, Y_test = make_dataset()
+    X_train, X_test, Y_train, Y_test = make_dataset()
 
-    initial_weights: list[np.ndarray] = model_stripped.trainable_weights
-    masks: list[np.ndarray] = mask_model_stripped.trainable_weights
-    train_losses: np.array = np.zeros(Y_train.shape[0])
-    train_accuracies: np.array = np.zeros(Y_train.shape[0])
-    test_losses: np.array = np.zeros(Y_test.shape[0])
-    test_accuracies: np.array = np.zeros(Y_test.shape[0])
+    initial_parameters: list[np.ndarray] = model.trainable_variables
+    masks: list[np.ndarray] = mask_model.trainable_variables
+
+    # Store the loss and accuracies at various points to use later in TrainingRound object
+    train_losses: np.array = np.zeros(num_epochs)
+    train_accuracies: np.array = np.zeros(num_epochs)
+    test_losses: np.array = np.zeros(num_epochs)
+    test_accuracies: np.array = np.zeros(num_epochs)
+
+    # Calculate the number of batches and create arrays to keep track of batch
+    # loss/accuracies while iterating over batches before it goes into training loss/accuracies
+    num_batches: int = int(np.ceil(len(X_train) / batch_size))
+    batch_losses: np.array = np.zeros(num_batches)
+    batch_accuracies: np.array = np.zeros(num_batches)
 
     for epoch in range(num_epochs):
-        
-        # Exit early if there are `patience` epochs without improvement
-        if local_patience >= patience:
-            print(f'Early stopping initiated')
-            break
-        
-        # Update model parameters for each point in the training set
-        for idx, (x, y) in enumerate(zip(X_train, Y_train)):
-            train_one_step(
-                model_stripped, 
-                mask_model_stripped, 
-                x, 
-                y, 
-                make_train_test_loss_accuracy,
-                optimizer=optimizer
+        for batch_index in range(num_batches):
+            # Calculate the lower/upper index for batch (assume data is shuffled)
+            low_index: int = batch_index * batch_size
+            high_index: int = (batch_index + 1) * batch_size
+            # Extract data to use for the batch
+            X_batch: np.ndarray = X_train[low_index:high_index]
+            Y_batch: np.ndarray = Y_train[low_index:high_index]
+
+            # Update model parameters for each point in the training set
+            loss, accuracy = train_one_step(
+                model, 
+                mask_model, 
+                X_batch, 
+                Y_batch, 
+                optimizer,
+                loss_fn,
             )
-            # print(f'Epoch {epoch + 1}, Iteration {idx + 1} Train Loss: {train_loss}, Train Accuracy: {train_accuracy}')
-            # train_losses[idx] = train_loss
-            # train_accuracies[idx] = train_accuracy
 
-        # Evaluate model on each point in the test set
-        for idx, (x_t, y_t) in enumerate(zip(X_test, Y_test)):
-            test_loss, test_accuracy = test_step(
-                model_stripped, 
-                optimizer, 
-                x_t, 
-                y_t,
-                test_loss,
-                test_accuracy,
-            )
-            print(f'Epoch {epoch + 1}, Iteration {idx + 1} Test Loss: {train_loss}, Test Accuracy: {train_accuracy}')
+            # Keep track of all the losses/accuracies within the epoch's batches here
+            batch_losses[batch_index] = loss
+            batch_accuracies[batch_index] = accuracy
 
-            test_losses[idx] = test_loss
-            test_accuracies[idx] = test_accuracy
+        # Set the overall epoch training loss/accuracy to mean of batches
+        train_losses[epoch] = np.mean(batch_losses)
+        train_accuracies[epoch] = np.mean(batch_accuracies)
 
-        # Display output
-        # print(f'Epoch {epoch + 1}, Train/Test Loss: {train_loss:.4f}/{test_loss:.4f}, Train/Test Accuracy: {train_accuracy:.4f}/{test_accuracy:.4f}')
-        # print(f'Total number of trainable parameters = {np.sum(count_params(model_stripped))}')
+        # Evaluate model on the test set using whole batch
+        test_loss, test_accuracy = test_step(
+            model, 
+            X_test, 
+            Y_test,
+        )
 
-        # Check for early stopping criteria
-        mean_test_loss: float = np.mean(test_losses)
-        if mean_test_loss < best_test_loss and (best_test_loss - mean_test_loss) >= minimum_delta:
-            # update 'best_test_loss' variable to lowest loss encountered so far
-            best_test_loss = mean_test_loss
-            # Reset the counter
-            local_patience = 0
-        else:  # there is no improvement in monitored metric 'val_loss'
-            local_patience += 1  # number of epochs without any improvement
+        test_losses[epoch] = test_loss
+        test_accuracies[epoch] = test_accuracy
 
-    final_weights: list[np.ndarray] = model_stripped.trainable_weights
+        if allow_early_stopping:
+            # Check for early stopping criteria using mean validation loss
+            mean_test_loss: float = np.mean(test_losses[:epoch + 1])
+            if mean_test_loss < best_test_loss and (best_test_loss - mean_test_loss) >= minimum_delta:
+                # update 'best_test_loss' variable to lowest loss encountered so far
+                best_test_loss = mean_test_loss
+                # Reset the counter
+                local_patience = 0
+            else:  # there is no improvement in monitored metric 'val_loss'
+                local_patience += 1  # number of epochs without any improvement
+
+            # Exit early if there are `patience` epochs without improvement
+            if local_patience >= patience:
+                print(f'Early stopping initiated')
+                break
+
+    final_parameters: list[np.ndarray] = model.trainable_variables
 
     # Compile training round data
-    round_data: TrainingRound = TrainingRound(pruning_step, initial_weights, final_weights, masks, train_losses, train_accuracies, test_losses, test_accuracies)
+    round_data: TrainingRound = TrainingRound(pruning_step, initial_parameters, final_parameters, masks, train_losses, train_accuracies, test_losses, test_accuracies)
 
-    return model_stripped, round_data
+    return model, round_data
     
 
 def train(
@@ -215,7 +247,6 @@ def train(
     model: tf.keras.Model, 
     mask_model: tf.keras.Model,
     make_dataset: callable, 
-    make_train_test_loss_accuracy: callable,
     num_epochs: int = C.TRAINING_EPOCHS,
     patience: int = C.PATIENCE,
     minimum_delta: float = C.MINIMUM_DELTA,
@@ -224,19 +255,18 @@ def train(
     """
     Function to perform training for a model.
 
-    :param pruning_step:     Integer value for the step in pruning. Defaults to 0.
-    :param model:            Model to optimize.
-    :param mask_model:       Model whose weights correspond to masks being applied.
-
-    :param make_dataset:     Function to produce the training/test sets.
-    :param num_epochs:       Number of epochs to train for.
-    :param patience:         Number of epochs which can be ran without improvement before calling early stopping.
-    :param minimum_delta:    Minimum increase to be considered an improvement.s
-    :param optimizer:        Optimizer to use during training.
+    :param random_seed:   Random seed being used.
+    :param pruning_step:  Integer value for the step in pruning. Defaults to 0.
+    :param model:         Model to optimize.
+    :param mask_model:    Model whose weights correspond to masks being applied.
+    :param make_dataset:  Function to produce the training/test sets.
+    :param num_epochs:    Number of epochs to train for.
+    :param patience:      Number of epochs which can be ran without improvement before calling early stopping.
+    :param minimum_delta: Minimum increase to be considered an improvement.s
+    :param optimizer:     Optimizer to use during training.
 
     :returns: Model, masked model, and training round objects with the final trained model and the training summary/.
     """
-
 
     # Run the training loop
     model, training_round = training_loop(
