88e4f463204e6afc0fd4c91fec62393e188d4a43
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-02-23 18:41:50 -0500

    Updating single feature analysis script.

--- scripts/meta/single_feature_analysis.py
+++ scripts/meta/single_feature_analysis.py
@@ -29,27 +29,25 @@ def make_plot(accuracies: npt.NDArray[np.float32], columns: List[str], batch_siz
     plt.savefig("single_feature_importances.png")
 
 if __name__ == "__main__":
-    df_path = os.path.join(basepath, "weightabase.pkl")
+    df_path = os.path.join(basepath, "mnist_weightabase.pkl")
     merged_df = pd.read_pickle(df_path)
-
-    i_features = ["l_sparsity", "l_rel_size", "li_prop_positive", "wi_std", "wi_perc", "wi_synflow", "wi_sign", "dense", "bias", "conv", "output"]
     epochs = 3
     batch_size = 256
     histories = []
-    # for index, feature in tqdm(enumerate(merged_df.columns)):
-    #     print(f"{feature} ({index + 1} / {len(merged_df.columns)})")
-    #     iX, iY = featurize_db(merged_df, [feature])
-    #     i_meta = create_meta(iX[0].shape)
-    #     histories.append(i_meta.fit(iX, iY, epochs=epochs, batch_size=batch_size, validation_split=0.2, shuffle=True))
+    for index, feature in tqdm(enumerate(merged_df.columns)):
+        print(f"{feature} ({index + 1} / {len(merged_df.columns)})")
+        iX, iY = featurize_db(merged_df, [feature])
+        i_meta = create_meta(iX[0].shape)
+        histories.append(i_meta.fit(iX, iY, epochs=epochs, batch_size=batch_size, validation_split=0.2, shuffle=True))
 
-    # accuracies = np.array(list(map(lambda x: x.history["accuracy"], histories)))
-    # with open("columns.txt", "w") as outfile:
-    #     outfile.write(str(merged_df.columns))
-    # np.save("accuracies", accuracies)
+    accuracies = np.array(list(map(lambda x: x.history["accuracy"], histories)))
+    with open("columns.txt", "w") as outfile:
+        outfile.write(str(merged_df.columns))
+    np.save("accuracies", accuracies)
 
-    accuracies = np.load("accuracies.npy")
-    with open("columns.txt", "r") as infile:
-        contents = infile.read()
-        columns = eval(contents[contents.index("["):contents.index("]")+1])
+    # accuracies = np.load("accuracies.npy")
+    # with open("columns.txt", "r") as infile:
+    #     contents = infile.read()
+    #     columns = eval(contents[contents.index("["):contents.index("]")+1])
     make_plot(accuracies, columns)
 
