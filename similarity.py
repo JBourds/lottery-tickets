b894478a5f0ff94dd12166ab27ebd0cf86f85d72
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-03-22 20:09:26 -0400

    Cosine similarity plot looks nice comparing two models.

--- similarity.py
+++ similarity.py
@@ -35,6 +35,7 @@ def df_path(steps: int) -> str:
         "lottery-tickets",
         "11-04-2024",
         "lenet_mnist_0_seed_5_experiments_1_batches_0.025_default_sparsity_lm_pruning_20241102-111614",
+        "data",
         f"merged_{steps}_steps.pkl"
     )
 
@@ -56,21 +57,18 @@ dataset = "mnist"
 a = arch.Architecture(architecture, dataset)
 steps = 10
 batch_size = 256
-merged_df = pd.read_pickle(df_path(steps))
-keys = ["e_num", "t_num", "l_num"]
-merged_df.sort_values(["w_num"], inplace=True)
 
 
 def model_cos_similarity(weights_0: List[npt.NDArray], weights_1: List[npt.NDArray]) -> List[List[float]]:
-    similarities = {"mean": [], "values": []}
+    weight_similarities = {"mean": [], "values": []}
     for w0, w1 in zip(weights_0, weights_1):
+        # Skip biases
         if len(w0.shape) == 1:
-            w0 = np.reshape(w0.ravel(), (-1, 1))
-            w1 = np.reshape(w1.ravel(), (-1, 1))
+            continue
         sim = cosine_similarity(w0, w1).ravel()
-        similarities["mean"].append(np.mean(np.abs(sim)))
-        similarities["values"].append(sim)
-    return similarities
+        weight_similarities["mean"].append(np.mean(np.abs(sim)))
+        weight_similarities["values"].append(sim)
+    return weight_similarities
 
 
 X_train, X_test, Y_train, Y_test = a.load_data()
@@ -79,17 +77,24 @@ m1 = a.get_model_constructor()()
 np.random.seed(1)
 m2 = a.get_model_constructor()()
 
-untrained_similarities = model_cos_similarity(
+untrained_weight_similarities = model_cos_similarity(
     m1.get_weights(), m2.get_weights())
-pprint(untrained_similarities)
+pprint(untrained_weight_similarities)
 
-masked_m1 = [w * (np.random.uniform(size=w.shape) < .95)
-             for w in m1.get_weights()]
-masked_m2 = [w * (np.random.uniform(size=w.shape) < .95)
-             for w in m2.get_weights()]
-masked_similarities = model_cos_similarity(
+
+def mask_overlap(masks_0: List[npt.NDArray], masks_1: List[npt.NDArray]) -> List[List[float]]:
+    overlaps = []
+    for m0, m1 in zip(masks_0, masks_1):
+        overlap = np.sum(m0 == m1)
+        overlaps.append(overlap / m0.size)
+    return overlaps
+
+
+masked_m1 = [w * m for w, m in zip(m1.get_weights(), mask_1)]
+masked_m2 = [w * m for w, m in zip(m2.get_weights(), mask_2)]
+masked_weight_similarities = model_cos_similarity(
     masked_m1, masked_m2)
-pprint(masked_similarities)
+pprint(masked_weight_similarities)
 
 m1.compile(optimizer="adam", loss=tf.keras.losses.CategoricalCrossentropy())
 m1.fit(X_train, Y_train, epochs=5)
@@ -98,40 +103,68 @@ m2.fit(X_train, Y_train, epochs=5)
 trained_m1 = m1
 trained_m2 = m2
 
-trained_similarities = model_cos_similarity(m1.get_weights(), m2.get_weights())
-pprint(trained_similarities)
+trained_weight_similarities = model_cos_similarity(
+    m1.get_weights(), m2.get_weights())
+pprint(trained_weight_similarities)
 
 epath = "/users/j/b/jbourde2/lottery-tickets/11-04-2024/lenet_mnist_0_seed_5_experiments_1_batches_0.025_default_sparsity_lm_pruning_20241102-111614"
 experiments = hist.get_experiments(epath)
-e0, e1, e2, e3, e4 = experiments
-similarities = []
-for index, (t0, t1) in enumerate(zip(e0, e1)):
-    t0.seed_weights = lambda x: x
-    t1.seed_weights = lambda x: x
-    t0_weights = [w * m for w, m in zip(t0.initial_weights, t0.masks)]
-    t1_weights = [w * m for w, m in zip(t1.initial_weights, t1.masks)]
-    similarities.append(model_cos_similarity(t0_weights, t1_weights))
+trials = list(map(list, experiments))
+for t in trials:
+    for x in t:
+        x.seed_weights = lambda x: x
+
+row_similarities = []
+col_similarities = []
+for (trials_0, trials_1) in combinations(trials, 2):
+    for t0, t1 in zip(trials_0, trials_1):
+        t0_weights = [w * m for w, m in zip(t0.initial_weights, t0.masks)]
+        t1_weights = [w * m for w, m in zip(t1.initial_weights, t1.masks)]
+        row_similarities.append(model_cos_similarity(t0_weights, t1_weights))
+        col_similarities.append(model_cos_similarity(
+            [w.T for w in t0_weights], [w.T for w in t1_weights]))
 
 # Build up plot DF
-data_dict = defaultdict(list)
-for step_num, step in enumerate(similarities):
+row_dict = defaultdict(list)
+for step_num, step in enumerate(row_similarities):
     # Remove bias layers
-    for layer, values in enumerate(step["values"][::2]):
-        data_dict["layer"].extend([layer] * len(values))
-        data_dict["step"].extend([step_num] * len(values))
-        data_dict["values"].extend(values)
-df = pd.DataFrame(data_dict)
+    for layer, values in enumerate(step["values"]):
+        row_dict["layer"].extend([layer] * len(values))
+        row_dict["step"].extend([step_num] * len(values))
+        row_dict["values"].extend(values)
+row_df = pd.DataFrame(row_dict)
+col_dict = defaultdict(list)
+for step_num, step in enumerate(col_similarities):
+    # Remove bias layers
+    for layer, values in enumerate(step["values"]):
+        col_dict["layer"].extend([layer] * len(values))
+        col_dict["step"].extend([step_num] * len(values))
+        col_dict["values"].extend(values)
+col_df = pd.DataFrame(col_dict)
 
-plot_df = df[(df.step == 0) | (df.step == df.step.max())]
-fig, layers = plt.subplots(
-    nrows=len(m1.get_weights()) // 2, sharex=True, figsize=(8, 6))
-fig.suptitle("Last Layer Cosine Similarity")
-for layer, ax in enumerate(layers):
-    layer_df = plot_df[plot_df.layer == layer]
-    sns.histplot(data=layer_df, x="values", hue="step",
+row_plot_df = row_df[(row_df.step == 0) | (row_df.step == row_df.step.max())]
+col_plot_df = col_df[(col_df.step == 0) | (col_df.step == col_df.step.max())]
+n_layers = len(m1.get_weights()) // 2
+fig, axes = plt.subplots(
+    nrows=n_layers, sharex=True, ncols=2, figsize=(8, 6))
+for layer in range(n_layers):
+    # Rows
+    row_layer_df = row_plot_df[row_plot_df.layer == layer]
+    ax = axes[layer, 0]
+    sns.histplot(data=row_layer_df, x="values", hue="step",
                  ax=ax)
     ax.get_legend().set_title("Pruning Step")
-    ax.set_title(f"Layer {layer}")
+    ax.set_title(f"Layer {layer} Rows")
+    ax.set_xlabel("Cosine Similarity")
+    ax.set_ylabel("Counts")
+    ax.set_yscale("log")
+    # Columns
+    col_layer_df = col_plot_df[col_plot_df.layer == layer]
+    ax = axes[layer, 1]
+    sns.histplot(data=col_layer_df, x="values", hue="step",
+                 ax=ax)
+    ax.get_legend().set_title("Pruning Step")
+    ax.set_title(f"Layer {layer} Cols")
     ax.set_xlabel("Cosine Similarity")
     ax.set_ylabel("Counts")
     ax.set_yscale("log")
@@ -148,15 +181,20 @@ def should_skip(key1: Tuple[int], key2: Tuple[int]) -> bool:
     return duplicate or incompatible
 
 
+merged_df = pd.read_pickle(df_path(steps))
+keys = ["e_num", "t_num"]
+merged_df.sort_values(["w_num"], inplace=True)
+layer_dimensions = {layer: shape for layer, shape in enumerate(
+    map(np.shape, a.get_model_constructor()().get_weights()))}
+
 # Are the flattened, masked arrays similar to each other?
 for key1, group1 in merged_df.groupby(keys):
     for key2, group2 in merged_df.groupby(keys):
         if should_skip(key1, key2):
             continue
         print(key1, key2)
-        x = np.reshape(group1["wi_val"] * group1["w_mask"], -1)
-        y = np.reshape(group2["wi_val"] * group2["w_mask"], -1)
-        pairwise_similarites[(key1, key2)] = cosine_similarity(x, y)
-
-with open("pairwise_similarities.json", "w") as outfile:
-    json.dump(pairwise_similarites, outfile)
+        x = [np.reshape(g["wf_val"], layer_dimensions[l])
+             for l, g in group1.groupby("l_num")]
+        y = [np.reshape(g["wf_val"], layer_dimensions[l])
+             for l, g in group2.groupby("l_num")]
+        pairwise_similarites[(key1, key2)] = model_cos_similarity(x, y)
