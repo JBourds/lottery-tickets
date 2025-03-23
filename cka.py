8cb21a24c5ba3a228a60e9e86354317858c92f4d
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-03-23 16:12:30 -0400

    Improving plots.

--- cka.py
+++ cka.py
@@ -88,15 +88,17 @@ def get_cka(epath: str, inputs: npt.NDArray, a: arch.Architecture, use_initial: 
     return np.array(linear_ckas)
 
 
-def plot_ckas(ckas: npt.NDArray, output: str):
+def plot_ckas(ckas: npt.NDArray, output: str, final_weights: bool):
     mean_ckas = np.mean(ckas, axis=0)
     std_ckas = np.std(ckas, axis=0)
-    pruning_steps = np.arange(mean_ckas.shape[0])
+    # TODO: Make this more robust depending on architecture
+    sparsities = 0.8 ** np.arange(mean_ckas.shape[0]) * 100
     plt.figure()
-    plt.title("CKA Score Over Time (Initial Weights)")
+    plt.title("CKA Score Over Time " +
+              ("(Final Weights)" if final_weights else "(Initial Weights)"))
     for layer_num in range(mean_ckas.shape[1]):
         bp.plot_aggregated_summary_ci(
-            pruning_steps,
+            sparsities,
             mean_ckas[:, layer_num],
             std_ckas[:, layer_num],
             init_linear_ckas.shape[0],
@@ -104,8 +106,10 @@ def plot_ckas(ckas: npt.NDArray, output: str):
             show_ci_legend=False,
             invert_x=False,
         )
+    plt.gca().invert_xaxis()
+    plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter())
     plt.legend()
-    plt.xlabel("Pruning Step")
+    plt.xlabel("Weights Remaining")
     plt.ylabel("CKA Score")
     plt.savefig(output)
 
@@ -121,6 +125,6 @@ X_train, X_test, Y_train, Y_test = a.load_data()
 
 inputs = X_test[:1000]
 init_linear_ckas = get_cka(epath, inputs, a, use_initial=True)
-plot_ckas(init_linear_ckas, "initial_cka_lenet_mnist.png")
-final_linear_ckas = get_cka(epath, inputs, a, use_initial=True)
-plot_ckas(final_linear_ckas, "final_cka_lenet_mnist.png")
+plot_ckas(init_linear_ckas, "initial_cka_lenet_mnist.png", final_weights=False)
+final_linear_ckas = get_cka(epath, inputs, a, use_initial=False)
+plot_ckas(final_linear_ckas, "final_cka_lenet_mnist.png", final_weights=True)
