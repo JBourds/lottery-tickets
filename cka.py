61f47ee24a7ea108ead8ecaff6baf6c051de0985
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-03-23 17:14:37 -0400

    Improving plots by putting them both on the same figure.

--- cka.py
+++ cka.py
@@ -52,7 +52,7 @@ def get_activations_model(a: arch.Architecture) -> Model:
     return activation_model
 
 
-def get_cka(epath: str, inputs: npt.NDArray, a: arch.Architecture, use_initial: bool = True) -> npt.NDArray:
+def get_cka(epath: str, inputs: npt.NDArray, a: arch.Architecture, use_initial: bool = True) -> Tuple[npt.NDArray, npt.NDArray]:
     experiments = hist.get_experiments(epath)
     trials = list(map(list, experiments))
     for t in trials:
@@ -88,30 +88,37 @@ def get_cka(epath: str, inputs: npt.NDArray, a: arch.Architecture, use_initial: 
     return np.array(linear_ckas)
 
 
-def plot_ckas(ckas: npt.NDArray, output: str, final_weights: bool):
-    mean_ckas = np.mean(ckas, axis=0)
-    std_ckas = np.std(ckas, axis=0)
-    # TODO: Make this more robust depending on architecture
-    sparsities = 0.8 ** np.arange(mean_ckas.shape[0]) * 100
-    plt.figure()
-    plt.title("CKA Score Over Time " +
-              ("(Final Weights)" if final_weights else "(Initial Weights)"))
-    for layer_num in range(mean_ckas.shape[1]):
-        bp.plot_aggregated_summary_ci(
-            sparsities,
-            mean_ckas[:, layer_num],
-            std_ckas[:, layer_num],
-            init_linear_ckas.shape[0],
-            legend=f"Layer {layer_num}",
-            show_ci_legend=False,
-            invert_x=False,
-        )
-    plt.gca().invert_xaxis()
-    plt.gca().xaxis.set_major_formatter(mtick.PercentFormatter())
-    plt.legend()
-    plt.xlabel("Weights Remaining")
-    plt.ylabel("CKA Score")
-    plt.savefig(output)
+def plot_ckas(init_ckas: npt.NDArray, final_ckas: npt.NDArray, output: str):
+    fig, (initial_ax, final_ax) = plt.subplots(ncols=2, sharey=True)
+    fig.suptitle("CKA Score Over Pruning")
+
+    def plot_cka(ax, cka: npt.NDArray, final: bool):
+        mean_ckas = np.mean(cka, axis=0)
+        std_ckas = np.std(cka, axis=0)
+        # TODO: Make this more robust depending on architecture
+        sparsities = 0.8 ** np.arange(mean_ckas.shape[0]) * 100
+        ax.set_title("Final Weights" if final else "Initial Weights")
+        plt.sca(ax)
+        for layer_num in range(mean_ckas.shape[1]):
+            bp.plot_aggregated_summary_ci(
+                sparsities,
+                mean_ckas[:, layer_num],
+                std_ckas[:, layer_num],
+                init_linear_ckas.shape[0],
+                legend=f"Layer {layer_num}",
+                show_ci_legend=False,
+                invert_x=False,
+            )
+        ax.invert_xaxis()
+        ax.xaxis.set_major_formatter(mtick.PercentFormatter())
+        ax.set_xlabel("Weights Remaining")
+        ax.set_ylabel("CKA Score")
+        ax.grid()
+        ax.legend()
+    plot_cka(initial_ax, init_ckas, False)
+    plot_cka(final_ax, final_ckas, True)
+    fig.tight_layout()
+    fig.savefig(output)
 
 
 reload(arch)
@@ -125,6 +132,6 @@ X_train, X_test, Y_train, Y_test = a.load_data()
 
 inputs = X_test[:1000]
 init_linear_ckas = get_cka(epath, inputs, a, use_initial=True)
-plot_ckas(init_linear_ckas, "initial_cka_lenet_mnist.png", final_weights=False)
 final_linear_ckas = get_cka(epath, inputs, a, use_initial=False)
-plot_ckas(final_linear_ckas, "final_cka_lenet_mnist.png", final_weights=True)
+
+plot_ckas(init_linear_ckas, final_linear_ckas, "cka_lenet_mnist.png")
